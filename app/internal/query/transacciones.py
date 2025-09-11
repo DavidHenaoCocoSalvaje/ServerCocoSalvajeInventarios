b32091418307e052637ab9a370c911f37970c83e
David <ia@cocosalvaje.co>
2025-09-11 16:56:17 -0500

    fix: statement

--- app/internal/query/transacciones.py
+++ app/internal/query/transacciones.py
@@ -1,18 +1,19 @@
 # app.internal.query.transacciones
 from datetime import timedelta
 from app.internal.gen.utilities import DateTz
-from app.internal.log import LogLevel, factory_logger
+
+# from app.internal.log import LogLevel, factory_logger
 from app.internal.query.base import BaseQuery
 from app.models.db.transacciones import (
     Pedido,
     PedidoCreate,
 )
 
-from sqlmodel import select, not_
+from sqlmodel import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-log_pedido_query = factory_logger('pedido_query', LogLevel.DEBUG, file=False)
+# log_pedido_query = factory_logger('pedido_query', LogLevel.DEBUG, file=False)
 
 
 class PedidoQuery(BaseQuery[Pedido, PedidoCreate]):
@@ -27,14 +28,12 @@ class PedidoQuery(BaseQuery[Pedido, PedidoCreate]):
     async def get_pendientes_facturar(self, session: AsyncSession) -> list[Pedido]:
         # factura_id = '' o q_intentos > 0
         # Se restan 5 minutos para evitar obtener pedidos que se están procesando en el momento.
-        datetime = DateTz.local() - timedelta(minutes=5)
+        datetime = DateTz.local_date() - timedelta(minutes=5)
         statement = (
             select(self.model_db)
-            .where(not_(self.model_db.factura_id == ''))
+            .where(self.model_db.factura_id.is_(None))  # type: ignore
             .where(self.model_db.q_intentos > 0)
             .where(self.model_db.fecha < datetime)
         )
-        sql = statement.compile(compile_kwargs={'literal_binds': True})
-        log_pedido_query.debug(sql)
         result = await session.execute(statement)
         return list(result.scalars().all()) or []
