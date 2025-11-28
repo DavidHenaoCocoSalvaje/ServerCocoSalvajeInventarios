416535665d4c100ac88f64e1d73f5814f92f80df
David <ia@cocosalvaje.co>
2025-11-28 09:09:28 -0500

    feat: nuevo modelo compra transacciones > Compra

--- app/models/db/transacciones.py
+++ app/models/db/transacciones.py
@@ -20,6 +20,13 @@ from app.internal.gen.utilities import DateTz
 class TransaccionBase(SQLModel):
     __table_args__ = {'schema': 'transaccion'}
     fecha: datetime = Field(sa_type=TIMESTAMP(timezone=True), default_factory=DateTz.local)  # type: ignore
+    factura_id: int | None = None  # Creación exitosa cuando se recibe número de factura
+    factura_numero: int | None = None  # Creación exitosa cuando se recibe número de factura
+    contabilizado: bool = False
+    pago: bool = False
+    q_intentos: int = Field(
+        sa_type=SMALLINT, default=3
+    )  # Determina los intentos de reprocesar si la transacción no está facturada/contabilizada
 
 
 class PedidoLogs(Enum):
@@ -29,18 +36,21 @@ class PedidoLogs(Enum):
 
 class PedidoCreate(TransaccionBase):
     numero: int | None = None  # Número de pedido shopify ej. #1234
-    factura_id: int | None = None  # Creación exitosa cuando se recibe número de factura
-    factura_numero: int | None = None  # Creación exitosa cuando se recibe número de factura
-    contabilizado: bool = False
-    pago: bool = False
     log: str | PedidoLogs | None = Field(sa_type=TEXT, default=None)
-    q_intentos: int = Field(
-        sa_type=SMALLINT, default=3
-    )  # Determina los intentos de reprocesar si el pedido no está facturado/contabilizado
 
 
 class Pedido(PedidoCreate, table=True):
     __tablename__ = 'pedidos'  # type: ignore
+    id: int = Field(primary_key=True, default=None)
+
+
+class CompraCreate(TransaccionBase):
+    numero_factura_proveedor: str | None = None  # Número de factura proveedor ej. #FV1234
+    log: str | PedidoLogs | None = Field(sa_type=TEXT, default=None)
+
+
+class Compra(CompraCreate, table=True):
+    __tablename__ = 'compras'  # type: ignore
     id: int = Field(primary_key=True, default=None)
 
 
